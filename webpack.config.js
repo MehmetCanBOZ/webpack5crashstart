9d9f0d686ec2f1f2b217b3e38aa19d4213c01680
MehmetCanBOZ <bozmehmetcan01@gmail.com>
2022-03-25 16:17:09 +0300

    W-4 add devserver pluging and css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: { bundle: path.resolve(__dirname, "src/index.js") },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[name][contenthash].js",
+    clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack",
+      filename: "index.html",
+      template: "src/template.html",
+    }),
+  ],
 };
